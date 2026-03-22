dafeb2b7a85aaeb90adb1578339008aa4725fadd
Sarvesh Singh <sarveshsingh101@gmail.com>
2026-03-22 20:41:39 +0530

    fix: rewrite http snippets using fmta to fix brace escaping

--- luasnippets/http.lua
+++ luasnippets/http.lua
@@ -1,93 +1,138 @@
-local ls  = require("luasnip")
-local s   = ls.snippet
-local t   = ls.text_node
-local i   = ls.insert_node
-local fmt = require("luasnip.extras.fmt").fmt
+local ls   = require("luasnip")
+local s    = ls.snippet
+local i    = ls.insert_node
+local t    = ls.text_node
+local fmta = require("luasnip.extras.fmt").fmta
+
+-- fmta uses <> as placeholders, << >> for literal < >
+-- {{ and }} are literal braces in the output — perfect for {{variable}} syntax
 
 return {
-  -- @var = value declaration
-  s("var", fmt("@{} = {}", { i(1, "baseUrl"), i(2, "http://localhost:5000") })),
+
+  -- @var = value
+  s("var", fmta("@<name> = <value>", {
+    name  = i(1, "baseUrl"),
+    value = i(2, "http://localhost:5000"),
+  })),
 
   -- ### separator
-  s("sep", { t("###") }),
+  s("sep", t("###")),
 
-  -- GET request
-  s("get", fmt([[
-GET {{{{{}}}}}/{}
+  -- GET
+  s("get", fmta([[
+GET {{<url>}}/<endpoint>
 Accept: application/json
-]], { i(1, "baseUrl"), i(2, "endpoint") })),
-
-  -- POST with JSON body
-  s("post", fmt([[
-POST {{{{{}}}}}/{}
-Content-Type: application/json
-Accept: application/json
-
-{{
-{}
-}}
-]], { i(1, "baseUrl"), i(2, "endpoint"), i(3, '  "key": "value"') })),
-
-  -- PUT with JSON body
-  s("put", fmt([[
-PUT {{{{{}}}}}/{}/{}
-Content-Type: application/json
-Accept: application/json
-
-{{
-{}
-}}
-]], { i(1, "baseUrl"), i(2, "endpoint"), i(3, "id"), i(4, '  "key": "value"') })),
-
-  -- PATCH with JSON body
-  s("patch", fmt([[
-PATCH {{{{{}}}}}/{}/{}
-Content-Type: application/json
-Accept: application/json
-
-{{
-{}
-}}
-]], { i(1, "baseUrl"), i(2, "endpoint"), i(3, "id"), i(4, '  "key": "value"') })),
-
-  -- DELETE request
-  s("del", fmt([[
-DELETE {{{{{}}}}}/{}/{}
-Accept: application/json
-]], { i(1, "baseUrl"), i(2, "endpoint"), i(3, "id") })),
-
-  -- Authorization Bearer header
-  s("auth", fmt("Authorization: Bearer {}", { i(1, "{{token}}") })),
-
-  -- JSON headers only
-  s("json", fmt([[
-Content-Type: application/json
-Accept: application/json]], {})),
-
-  -- Full request file setup block
-  s("setup", fmt([[
-@{} = {}
-@{} = {}
-
-###
-]], { i(1, "baseUrl"), i(2, "http://localhost:5000"), i(3, "token"), i(4, "your-token-here") })),
+]], {
+    url      = i(1, "baseUrl"),
+    endpoint = i(2, "api/endpoint"),
+  })),
 
   -- GET with Authorization
-  s("getauth", fmt([[
-GET {{{{{}}}}}/{}
+  s("getauth", fmta([[
+GET {{<url>}}/<endpoint>
 Accept: application/json
-Authorization: Bearer {{{{{}}}}}
-]], { i(1, "baseUrl"), i(2, "endpoint"), i(3, "token") })),
+Authorization: Bearer {{<token>}}
+]], {
+    url      = i(1, "baseUrl"),
+    endpoint = i(2, "api/endpoint"),
+    token    = i(3, "token"),
+  })),
 
-  -- POST with Authorization and JSON body
-  s("postauth", fmt([[
-POST {{{{{}}}}}/{}
+  -- POST with JSON body
+  s("post", fmta([[
+POST {{<url>}}/<endpoint>
 Content-Type: application/json
 Accept: application/json
-Authorization: Bearer {{{{{}}}}}
 
-{{
-{}
-}}
-]], { i(1, "baseUrl"), i(2, "endpoint"), i(3, "token"), i(4, '  "key": "value"') })),
+{
+  <body>
+}
+]], {
+    url      = i(1, "baseUrl"),
+    endpoint = i(2, "api/endpoint"),
+    body     = i(3, '"key": "value"'),
+  })),
+
+  -- POST with Authorization + JSON body
+  s("postauth", fmta([[
+POST {{<url>}}/<endpoint>
+Content-Type: application/json
+Accept: application/json
+Authorization: Bearer {{<token>}}
+
+{
+  <body>
+}
+]], {
+    url      = i(1, "baseUrl"),
+    endpoint = i(2, "api/endpoint"),
+    token    = i(3, "token"),
+    body     = i(4, '"key": "value"'),
+  })),
+
+  -- PUT with JSON body
+  s("put", fmta([[
+PUT {{<url>}}/<endpoint>/<id>
+Content-Type: application/json
+Accept: application/json
+
+{
+  <body>
+}
+]], {
+    url      = i(1, "baseUrl"),
+    endpoint = i(2, "api/endpoint"),
+    id       = i(3, "id"),
+    body     = i(4, '"key": "value"'),
+  })),
+
+  -- PATCH with JSON body
+  s("patch", fmta([[
+PATCH {{<url>}}/<endpoint>/<id>
+Content-Type: application/json
+Accept: application/json
+
+{
+  <body>
+}
+]], {
+    url      = i(1, "baseUrl"),
+    endpoint = i(2, "api/endpoint"),
+    id       = i(3, "id"),
+    body     = i(4, '"key": "value"'),
+  })),
+
+  -- DELETE
+  s("del", fmta([[
+DELETE {{<url>}}/<endpoint>/<id>
+Accept: application/json
+]], {
+    url      = i(1, "baseUrl"),
+    endpoint = i(2, "api/endpoint"),
+    id       = i(3, "id"),
+  })),
+
+  -- Authorization header only
+  s("auth", fmta("Authorization: Bearer {{<token>}}", {
+    token = i(1, "token"),
+  })),
+
+  -- JSON headers
+  s("json", t({
+    "Content-Type: application/json",
+    "Accept: application/json",
+  })),
+
+  -- Full file setup block
+  s("setup", fmta([[
+@<urlName> = <urlValue>
+@<tokenName> = <tokenValue>
+
+###
+]], {
+    urlName    = i(1, "baseUrl"),
+    urlValue   = i(2, "http://localhost:5000"),
+    tokenName  = i(3, "token"),
+    tokenValue = i(4, "your-token-here"),
+  })),
 }
